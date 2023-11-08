07a3d371f54fac3261c02d47485c4d9cbb7e6bee
JorgeArroyo33 <jorge.arroyo@alu.uclm.es>
2023-11-08 22:50:56 +0100

    nueva

--- prubas/src/prubas/main.java
+++ prubas/src/prubas/main.java
@@ -7,8 +7,9 @@ public class main {
 		
 		int matriz [][] =new int[1][2];
 		
-		System.out.println(matriz[2].length);
+		System.out.println(matriz[1].length);
 		//se actualiza o que putas pollas pasa
+		
 		
 	}
 }
