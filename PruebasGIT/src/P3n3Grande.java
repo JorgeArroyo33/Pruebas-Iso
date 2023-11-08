d475d20992ddc109bf785c2e2558c53e381cd6d3
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-11-08 17:56:04 +0100

    Cambiado bucle For por bucle While

--- PruebasGIT/src/P3n3Grande.java
+++ PruebasGIT/src/P3n3Grande.java
@@ -1,8 +1,14 @@
 public class P3n3Grande {
 	public static void main(String[] args) {
 		System.out.println("Hello P3n3Grande");
-		for(int i = 0; i < 10; i++) {
+		int variable = 0;
+		while(variable < 10) {
 			System.out.println("Hola pene grandeeee 69 jejejej.");
+			variable++;
 		}
+		
+		/*for(int i = 0; i < 10; i++) {
+			System.out.println("Hola pene grandeeee 69 jejejej.");
+		}*/
 	}
 }
