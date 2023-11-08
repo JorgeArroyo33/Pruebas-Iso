f98df1d1692c850d38b98917a5ca28465af5ed2a
JorgeArroyo33 <jorge.arroyo@alu.uclm.es>
2023-11-08 21:34:18 +0100

    subido

---
+++ prubas/src/prubas/main.java
@@ -0,0 +1,14 @@
+package prubas;
+
+public class main {
+
+	
+	public static void main(String [] args) {
+		
+		int matriz [][] =new int[1][2];
+		
+		System.out.println(matriz[1].length);
+		
+		
+	}
+}
