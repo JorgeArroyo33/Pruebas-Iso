516eca7a24ea7eaf90c23e918b95cfce87755cc4
SergiooCoorne <Sergio.Cornejo@alu.uclm.es>
2023-11-08 17:53:10 +0100

    Añadido bucle en el que se saluda al pene grande 69

--- PruebasGIT/src/P3n3Grande.java
+++ PruebasGIT/src/P3n3Grande.java
@@ -1,5 +1,8 @@
 public class P3n3Grande {
 	public static void main(String[] args) {
 		System.out.println("Hello P3n3Grande");
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Hola pene grandeeee 69 jejejej.");
+		}
 	}
 }
