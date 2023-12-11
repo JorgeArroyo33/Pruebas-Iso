b35abffcdc33e761d6850d27c6809473f2deedfa
JorgeArroyo33 <146016319+JorgeArroyo33@users.noreply.github.com>
2023-12-11 16:09:28 +0100

    Delete ASISTENCIA directory

--- ASISTENCIA/src/main/java/Organizador/Organizador_Eventos/Organizador_Domain/Presencialidad.java
+++
@@ -1,7 +0,0 @@
-package Abrir_Evento.Organizador.Organizador_Eventos.Organizador_Domain;
-
-public enum Presencialidad {
-	Presencial_Geolocalizar,
-	Presencial_Bluetooth,
-	Online
-}
--- ASISTENCIA/src/main/java/Organizador/Organizador_Eventos/Organizador_Presentacion/JFrameInicio_Organizador.java
+++
@@ -1,4 +0,0 @@
-package Abrir_Evento.Organizador.Organizador_Eventos.Organizador_Presentacion;
-
-public class JFrameInicio_Organizador {
-}
